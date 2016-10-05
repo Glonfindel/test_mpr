36b75350d50b06f166eb0ad702546be6bc910af4
student <student@virtualbox>
2016-10-05 15:32:10 +0200

    Add komentarz

--- gitDemo1/src/main/java/gitDemo/gitDemo1/App.java
+++ gitDemo1/src/main/java/gitDemo/gitDemo1/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+    	// Komentarz
         System.out.println( "Hello Karol!" );
         System.out.println( ":)!" );
     }
